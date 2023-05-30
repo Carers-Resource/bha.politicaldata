ed26cdfd19afc617bbc1a02961f99df69c410d60
Gav Massingham (Carers' Resource) <web@carersresource.org>
2023-05-30 16:38:33 +0100

    Add more missing semicolons.

--- politicaldata.php
+++ politicaldata.php
@@ -166,9 +166,10 @@ function politicaldata_civicrm_postCommit($op, $objectName, $id, &$objectref)
 
   if (array_key_exists($councilID, $politicaldata['areas'])) {
       $countycouncil = $politicaldata['areas'][$councilID]['name'];
-      // if $politicaldata['areas'][$councilID]['generation_low'] =< 48 {
-      //         $politicaldata = curlMapItData($url . '&generation=48' )
-      //                        }
+      if $politicaldata['areas'][$councilID]['generation_low'] =< 48 {
+              $url .= '&generation=48';
+              $politicaldata = curlMapItData($url);
+          }
   }
 
   if (politicaldata_array_key_exists_r('county', $politicaldata)) {
